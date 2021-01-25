aa7b3bb1e4809a15254782c47a12fab3de17adea
Ori Markowitz <ori.markowitz@gmail.com>
2021-01-25 17:44:54 -0500

    Ben and I finished sign up and login features

--- index.js
+++ index.js
@@ -21,19 +21,30 @@ const getGuides = (id) => {
     if (!id) {
         return fetch(`http://localhost:3000/guides`)
             .then(resp => resp.json())
-            .then(guideArray => {
-                renderGuides(guideArray)
-            })
     } else {
         return fetch(`http://localhost:3000/guides/${id}`)
             .then(resp => resp.json())
     }
 }
 
+const signupUser = (newUserObj) => {
+    return fetch(`http://localhost:3000/users`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUserObj)
+    })
+        .then(resp => resp.json())
+}
+
 //Event Listener
 document.addEventListener("DOMContentLoaded", function() {
     navBar.querySelector("#home").classList.add("active")
     getGuides()
+        .then(guideArray => {
+            renderGuides(guideArray)
+        })
 })
 
 navBar.addEventListener("click", function (e) {
@@ -44,6 +55,9 @@ navBar.addEventListener("click", function (e) {
         e.target.classList.add("active")
         mainBox.innerHTML = null
         getGuides()
+            .then(guideArray => {
+                renderGuides(guideArray)
+            })
     } else if (e.target.id === "write-guide") {
         e.preventDefault()
         document.querySelectorAll(".left-nav-items").forEach(el => el.classList.remove('active'))
@@ -57,23 +71,77 @@ navBar.addEventListener("click", function (e) {
         e.target.classList.add("active")
         console.log("This is how it works!")
     } else if (e.target.id === "sign-up") {
-        e.preventDefault()
-        document.querySelectorAll(".left-nav-items").forEach(el => el.classList.remove('active'))
-        document.querySelectorAll(".right-nav-items").forEach(el => el.classList.remove('active'))
-        e.target.classList.add("active")
-        console.log("Signed up!")
+        if (e.target.innerText === "Logout") {
+            e.preventDefault()
+            logoutUser()
+            document.querySelectorAll(".left-nav-items").forEach(el => el.classList.remove('active'))
+            document.querySelectorAll(".right-nav-items").forEach(el => el.classList.remove('active'))
+            document.querySelector("#home").classList.add("active")
+            getGuides()
+                .then(guidesArray => {
+                    renderGuides(guidesArray)
+                })
+            console.log("Signed out!")
+        } else {
+            e.preventDefault()
+            document.querySelectorAll(".left-nav-items").forEach(el => el.classList.remove('active'))
+            document.querySelectorAll(".right-nav-items").forEach(el => el.classList.remove('active'))
+            e.target.classList.add("active")
+            renderSignupPage()
+        }
     } else if (e.target.id === "login") {
-        e.preventDefault()
-        document.querySelectorAll(".left-nav-items").forEach(el => el.classList.remove('active'))
-        document.querySelectorAll(".right-nav-items").forEach(el => el.classList.remove('active'))
-        e.target.classList.add("active")
-        renderLoginPage()
-        console.log("Logged in!")
+        if (e.target.innerText === "Login") {
+            e.preventDefault()
+            document.querySelectorAll(".left-nav-items").forEach(el => el.classList.remove('active'))
+            document.querySelectorAll(".right-nav-items").forEach(el => el.classList.remove('active'))
+            e.target.classList.add("active")
+            renderLoginPage()
+        } else {
+            e.preventDefault()
+            document.querySelectorAll(".left-nav-items").forEach(el => el.classList.remove('active'))
+            document.querySelectorAll(".right-nav-items").forEach(el => el.classList.remove('active'))
+            e.target.classList.add("active")
+            renderUserPage(selectedUser)
+            console.log("My Profile!")
+        }
     }
 })
 
 
 //Event Handlers
+const loginUser = (username) => {
+    getUsers()
+        .then(usersArray => {
+            let user = usersArray.filter(user => {
+                return user.username === username
+            })
+            if (user[0]) {
+                selectedUser = user[0]
+                navBar.querySelector("#login").classList.remove("active")
+                navBar.querySelector("#login").innerText = `${selectedUser.username}'s Profile`
+                navBar.querySelector("#sign-up").innerText = "Logout"
+                navBar.querySelector("#home").classList.add("active")
+                getGuides()
+                    .then(guideArray => {
+                        renderGuides(guideArray)
+                    })
+
+            } else {
+                window.alert("No User with that Username was found!")
+                renderLoginPage()
+            }
+        })
+}
+
+const logoutUser = () => {
+    selectedUser = {}
+    navBar.querySelector("#sign-up").innerText = `Sign Up`
+    navBar.querySelector("#login").innerText = "Login"
+}
+
+
+
+
 
 
 //Renders
@@ -95,6 +163,7 @@ const renderGuide = (guideObj) => {
 
 
 function renderGuides(guidesArray) {
+    mainBox.innerHTML = null
     guidesArray.forEach(renderGuide)
 }
 
@@ -177,10 +246,120 @@ const renderLoginPage = () => {
     loginButton.value = "Login"
     loginForm.addEventListener("submit", function (e) {
         e.preventDefault()
-        console.log("Logged in!!!")
+        loginUser(e.target.username.value)
     })
     loginForm.append(usernameInput, loginButton)
     mainBox.append(loginForm)
+    
+}
+
+const renderUserPage = (selectedUser) => {
+    mainBox.innerHTML = null
+    const userBox = document.createElement("div")
+    userBox.className = "user-box"
+    userBox.dataset.id = selectedUser.id
+    const userImage = document.createElement("img")
+    userImage.className = "user-image"
+    userImage.src = selectedUser.img_url
+    userImage.alt = selectedUser.name
+    const editButton = document.createElement("button")
+    editButton.innerText = "Edit"
+    editButton.addEventListener("click", function() {
+        console.log("Edit!")
+    })
+    const deleteButton = document.createElement("button")
+    deleteButton.innerText = "Delete"
+    deleteButton.addEventListener("click", function() {
+        console.log("Delete!")
+    })
+    const name = document.createElement("h1")
+    name.innerText = selectedUser.name
+    const guidesBox = document.createElement("div")
+    const guidesLabel = document.createElement("h2")
+    guidesLabel.innerText = "Guides: "
+    guidesBox.append(guidesLabel)
+    getGuides()
+        .then(guidesArray => {
+            let userGuides = guidesArray.filter(guide => {
+                return guide.user_id === selectedUser.id
+            })
+            userGuides.forEach(function (guide) {
+                const userGuideCard = document.createElement("div")
+                userGuideCard.className = "user-card"
+                const title = document.createElement("h2")
+                const author = document.createElement("h3")
+                title.innerText = guide.title
+                userGuideCard.dataset.id = guide.id
+                userGuideCard.addEventListener("click", function() {
+                    selectedGuide = guide
+                    renderGuideShow(guide)
+                } )
+                userGuideCard.append(title)
+                guidesBox.append(userGuideCard)
+                userBox.append(guidesBox)
+            })
+    })
+    
+    const wallet = document.createElement("h3")
+    wallet.innerText = "Wallet: "
+    const walletAmount = document.createElement("p")
+    walletAmount.innerText = "3 Tokens"
+    userBox.append(userImage, editButton, deleteButton, name, wallet, walletAmount)
+    mainBox.append(userBox)
+}
+
+const renderSignupPage = () => {
+    mainBox.innerHTML = null
+    const signupForm = document.createElement("form")
+    signupForm.id = "signup-form"
+    const nameLabel = document.createElement("label")
+    nameLabel.innerText = "Name: "
+    const nameInput = document.createElement("input")
+    nameInput.type = "text"
+    nameInput.name = "name"
+    nameInput.id = "name"
+    const emailLabel = document.createElement("label")
+    emailLabel.innerText = "Email: "
+    const emailInput = document.createElement("input")
+    emailInput.type = "text"
+    emailInput.name = "email"
+    emailInput.id = "email"
+    const imgLabel = document.createElement("label")
+    imgLabel.innerText = "Image: "
+    const imgInput = document.createElement("input")
+    imgInput.type = "text"
+    imgInput.name = "img_url"
+    imgInput.id = "img_url"
+    const usernameLabel = document.createElement("label")
+    usernameLabel.innerText = "Username: "
+    const usernameInput = document.createElement("input")
+    usernameInput.type = "text"
+    usernameInput.name = "username"
+    usernameInput.id = "username"
+    const signupButton = document.createElement("input")
+    signupButton.type = "submit"
+    signupButton.value = "Sign Up"
+    signupForm.addEventListener("submit", function (e) {
+        e.preventDefault()
+        let newUserObj = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            img_url: e.target.img_url.value,
+            username: e.target.username.value
+        }
+        signupUser(newUserObj)
+            .then(userObj => {
+                selectedUser = userObj
+                navBar.querySelector("#sign-up").classList.remove("active")
+                navBar.querySelector("#login").innerText = `${selectedUser.username}'s Profile`
+                navBar.querySelector("#sign-up").innerText = "Logout"
+                navBar.querySelector("#login").classList.add("active")
+                renderUserPage(selectedUser)
+
+            })
+    })
+    signupForm.append(usernameLabel, usernameInput, nameLabel, nameInput, emailLabel, emailInput, imgLabel, imgInput, signupButton)
+    mainBox.append(signupForm)
     
 }
 
